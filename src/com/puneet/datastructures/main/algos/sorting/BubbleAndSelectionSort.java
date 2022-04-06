10a66f753c7c58dfd4f6d89fcbc4c94855ca3c8b
PuKumar <pukumar@lb.com>
2022-04-06 08:22:38 -0400

    Exercises of Selection and Bubble Sort

--- src/com/puneet/datastructures/main/algos/sorting/BubbleAndSelectionSort.java
+++ src/com/puneet/datastructures/main/algos/sorting/BubbleAndSelectionSort.java
@@ -2,13 +2,39 @@ package com.puneet.datastructures.main.algos.sorting;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class BubbleAndSelectionSort {
 
     public static void main(String[] args) {
-        int[] arr = {3,1, 5, 4, 2};
+        int[] arr = {3, 1, 5, 4, 2};
         System.out.println(Arrays.toString(arr));
-        bubble(arr);
+       // bubble(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find max item in current array and swap it with last element
+            int last = arr.length - i - 1;
+            int max = getMaxIndex(arr, 0, last);
+            swap(arr, max, last);
+        }
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 
     static void bubble(int[] arr) {
@@ -21,9 +47,7 @@ public class BubbleSort {
                 // swap if the item is smaller than previous item
                 if (arr[j] < arr[j - 1]) {
                     // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    swap(arr, j, j-1);
                     swapped = true;
                 }
             }
